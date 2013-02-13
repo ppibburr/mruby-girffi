05df4182e7dcd411c5ce6f6b938821f7bfa07e22
ppibburr <tulnor33@gmail.com>
2013-02-13 14:12:00 +0000

    match mruby fixes, use CFunc.libcall

--- mrblib/girbind.rb
+++ mrblib/girbind.rb
@@ -103,18 +103,6 @@ module FFI
     CFunc::call(CFunc::Pointer, get_dlopen(),lib,true )
   end
 
-  class Func
-    attr_accessor :result_type,:arguments_type,:name
-    def initialize name
-      @name = name
-    end
-
-    def call *o
-      @cf ||= CFunc::define_function(result_type,name,*arguments_type)
-      @cf.call *o
-    end
-  end
-
   def _ffi_lib_
     @ffi_lib
   end
@@ -124,52 +112,28 @@ module FFI
     @library = get_library_handle_for lib
   end
 
-  @@funcs = {}  
-
-  def self.funcs
-    @@funcs
-  end
-
   def call_func name,types,*o
     o.each_with_index do |q,i|
       if q.respond_to?(:ffi_ptr)
         o[i] = q.ffi_ptr
       elsif q.is_a?(Numeric)
-#        # p :rbum2cnum
           o[i] = FFI.rnum2cnum(q,types[0][i])
       elsif o.is_a?(Proc)
         exit
       end
     end
 
-    if !(f=FFI::Lib.funcs[name])
-      types = [[],nil] if types.length == 0
-      name=name.to_s
-      ptr = CFunc::call(CFunc::Pointer, "dlsym", @library, name)
-      f=CFunc::FunctionPointer.new(ptr)
-     # f = Func.new name
-      f.result_type = find_type(types.last)
-      ta=[]
-      types[0].each do |t|
-        ta << find_type(t)
-        
-      end
-      f.arguments_type=ta
-  #;exit
-      FFI::Lib.funcs[name] = f
-    end
-    
-    f=FFI::Lib.funcs[name]
-    #name,f
-  #  p [].push(result_type,@ffi_lib,name,*o)
 
-    r=CFunc::libcall(f.result_type,@ffi_lib,name,*o)
+    name=name.to_s
+
+    result_type = find_type(types.last)
+
+
+    r=CFunc::libcall(result_type,@ffi_lib,name,*o)
 
     if types.last == :bool
-      p :kk if !r
       r.value == 1
     else
-      #p "";p ""
       r
     end
   end
@@ -200,36 +164,21 @@ module FFI
     @@callbacks
   end
 
-  @@cnt = 0
-  $b = []
-
-  module K
-  end
-
   def attach_function nameq,*types
     this = self
     nameq = nameq.to_s.clone
 
-    @@cnt+=1
-    $b << b=Proc.new do |*o|
+    b=Proc.new do |*o|
       o.each_with_index do |q,i|
         if q.respond_to?(:ffi_ptr)
           o[i] = q.ffi_ptr
-        elsif o.is_a?(Proc)
-          exit
         end
       end
       
       this.call_func(nameq,types,*o)
     end
 
-    if !@ins
-     (@ins ||=(class << self;self;end))
-     extend K
-    end
-
-    i = $b.length-1
-    K.define_method nameq,&$b[i]
+    class << self;self;end.define_method nameq,&b
   end
  end
 end
